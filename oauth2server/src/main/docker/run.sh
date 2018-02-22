29552b7a7134225249851f864aaaad805e24e173
Liem Nguyen <liem_m_nguyen@hpe.com>
2018-02-22 11:48:54 -0800

    Added a poorman's OAuth2 server

--- oauth2server/src/main/docker/run.sh
+++ oauth2server/src/main/docker/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "********************************************************"
-echo "Starting OAuth2 Server with Configuration Service :  $CONFIGSERVER_URI";
+echo "Starting OAuth2 Server";
 echo "********************************************************"
 java -jar /usr/local/oauth2server/@project.build.finalName@.jar
