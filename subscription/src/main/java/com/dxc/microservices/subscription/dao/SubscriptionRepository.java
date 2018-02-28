94bcced3c2567b8d8b9e8d898e418693c966dd92
Liem Nguyen <liem_m_nguyen@hpe.com>
2018-02-28 12:46:58 -0800

    Added JWT authentication. Only ROLE_SUBSCRIBER can modify subscription

--- subscription/src/main/java/com/dxc/microservices/subscription/dao/SubscriptionRepository.java
+++ subscription/src/main/java/com/dxc/microservices/subscription/dao/SubscriptionRepository.java
@@ -5,10 +5,16 @@ import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.dxc.microservices.subscription.model.Subscription;
 
 @RepositoryRestResource(collectionResourceRel = "subscriptions", path = "subscriptions")
 public interface SubscriptionRepository extends PagingAndSortingRepository<Subscription, Long> {
     List<Subscription> findByCustomerId(@Param("customerId") int customerId);
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    @PreAuthorize("hasRole('ROLE_SUBSCRIBER')")
+    Subscription save(Subscription subscription);
 }
---
+++ subscription/src/main/java/com/dxc/microservices/subscription/security/WebSecurity.java
@@ -0,0 +1,36 @@
+package com.dxc.microservices.subscription.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.dxc.microservices.subscription.JWTAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    
+    // Add JWT Authorization filter
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .anyRequest().authenticated().and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+}
