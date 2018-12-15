8424eb2cf49d24efe04a006c43acd7fa5c78c208
Liem Nguyen <liem.m.nguyen@gmail.com>
2018-12-14 16:38:31 -0800

    Added Kubernetes deployment (tested on Azure)

--- billing/src/main/docker/run.sh
+++ billing/src/main/docker/run.sh
@@ -5,6 +5,4 @@ echo "Starting Billing Service with Configuration Service: $CONFIGSERVER_URI";
 echo "********************************************************"
 java -Dspring.cloud.config.uri=$CONFIGSERVER_URI                       \
      -Dspring.profiles.active=$PROFILE                                 \
-     -Dspring.cloud.stream.kafka.binder.zkNodes=$KAFKASERVER_URI       \
-     -Dspring.cloud.stream.kafka.binder.brokers=$ZKSERVER_URI          \
      -jar /usr/local/billingservice/@project.build.finalName@.jar
--- subscription/src/main/docker/run.sh
+++ subscription/src/main/docker/run.sh
@@ -5,6 +5,4 @@ echo "Starting Subscription Server with Configuration Service: $CONFIGSERVER_URI
 echo "********************************************************"
 java -Dspring.cloud.config.uri=$CONFIGSERVER_URI                       \
      -Dspring.profiles.active=$PROFILE                                 \
-     -Dspring.cloud.stream.kafka.binder.zkNodes=$KAFKASERVER_URI       \
-     -Dspring.cloud.stream.kafka.binder.brokers=$ZKSERVER_URI          \
      -jar /usr/local/subscriptionservice/@project.build.finalName@.jar
